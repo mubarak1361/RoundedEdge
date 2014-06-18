5f94f425de5b469cc71fdbecc85b2dce94658c45
Mubarak <disfrutarz@gmail.com'>
2014-06-18 23:02:54 +0530

    marquee subtitle

--- RoundEdge/src/com/codedesk/roundedge/MainActivity.java
+++ RoundEdge/src/com/codedesk/roundedge/MainActivity.java
@@ -2,7 +2,10 @@ package com.codedesk.roundedge;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.Resources;
+import android.text.TextUtils.TruncateAt;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,22 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		int subtitleId = Resources.getSystem().
+				getIdentifier("action_bar_subtitle", "id", "android");
+				if(subtitleId==0)
+				 subtitleId=com.actionbarsherlock.R.id.abs__action_bar_subtitle;
+				TextView mAppSubtitle=(TextView)findViewById(subtitleId);
+				mAppSubtitle.setEllipsize(TruncateAt.MARQUEE);
+				    mAppSubtitle.setMarqueeRepeatLimit(1);
+				    mAppSubtitle.setFocusable(true);
+				    mAppSubtitle.setFocusableInTouchMode(true);
+				    mAppSubtitle.requestFocus();
+				    mAppSubtitle.setSingleLine(true);
+				    mAppSubtitle.setSelected(true);
+				    mAppSubtitle.setMarqueeRepeatLimit(-1);
+		
+		
 	}
 
 	@Override
